788a730dd24c8bce7a8630e6ca17b8c0d45470dd
Adam Nguyen <anguyen@costar.com>
2025-02-09 23:30:42 -0500

    pp

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ForDiana } from './components/forDiana'
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/homepage" element={<Remainder />} />
-                <Route path="/valentine" element={<ForDiana />} />
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <Remainder />
+        </div>
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route path="/homepage" element={<Remainder />} />
+        //         <Route path="/valentine" element={<ForDiana />} />
+        //     </Routes>
+        // </BrowserRouter>
     )
 }
 
